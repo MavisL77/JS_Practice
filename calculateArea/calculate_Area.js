36a5d0e1517876e5ca0909093c8f73d7e4a0e1e9
Mavis <mavisliu12138@gmail.com>
2025-08-09 06:01:53 -0400

    Practice and Recap

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,4 +9,14 @@ function calculateArea() {
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function groceryTracker() {
+    Amount_1 = parseFloat(document.getElementById('Amount_1').value);
+    Amount_2 = parseFloat(document.getElementById('Amount_2').value);
+    Amount_3 = parseFloat(document.getElementById('Amount_3').value);
+   
+    let amount = Amount_1 + Amount_2 + Amount_3;
+    document.getElementById('total_amount').innerText = `The total amount is: ${amount}`;
+}
+
+
 
