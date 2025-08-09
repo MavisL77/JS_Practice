183b3dc958ca85c936640963bf795c12524d1f8f
Mavis <mavisliu12138@gmail.com>
2025-08-09 10:20:17 -0400

    Practice and Recap

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,8 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
+
 
     alert('Thank you for your valuable feedback')
 
@@ -19,6 +21,8 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;  
+    document.getElementById('userNameExp').innerHTML = username; 
+    document.getElementById('userExperience').innerHTML = experience;  
     
 }
 
