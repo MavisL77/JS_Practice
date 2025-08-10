5fb2797c1ff7666d47ed55796701bb6cd162560a
Mavis <mavisliu12138@gmail.com>
2025-08-09 23:25:00 -0400

    Practice and Recap

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -2,8 +2,13 @@ const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
+const breakfastMenu_2 = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+const breakfastMenuItemsHTML_2 = breakfastMenu_2.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('breakfastMenuItems_2').innerHTML = breakfastMenuItemsHTML_2;
 
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
